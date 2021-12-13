c2f4323c977510186413657b9d1ea0f319e90d99
Yaser7860 <90189815+Yasser-Slaymen@users.noreply.github.com>
2021-12-14 00:26:18 +0100

    script js

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+const btn=document.querySelector(".mobile-nav-toggle")
+const nav=document.querySelector(".nav-baar")
+
+btn.addEventListener("click",function(){
+    nav.classList.toggle("show");
+
+});
