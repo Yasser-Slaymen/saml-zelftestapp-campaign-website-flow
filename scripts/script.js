b73ca1048943ce11ab5ba8c20c167c13edcd4e27
Yaser7860 <90189815+Yasser-Slaymen@users.noreply.github.com>
2021-12-14 23:28:58 +0100

    scrolling function

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,25 @@ btn.addEventListener("click",function(){
     nav.classList.toggle("show");
 
 });
+
+const boxes=document.querySelectorAll(".box")
+window.addEventListener(("scroll"),checkBoxes)
+checkBoxes()
+
+function checkBoxes(){
+ const triggerBottom=window.innerHeight /5*4
+
+ boxes.forEach(box => {
+     const boxTop=box.getBoundingClientRect().top
+
+     if (boxTop<triggerBottom) {
+         box.classList.add("shows")
+         
+     } else {
+         box.classList.remove("shows")
+         
+     }
+     
+    });
+
+}
