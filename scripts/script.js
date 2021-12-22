a15536c3a03b70c5a45c52fe36571f8dfe8f3491
Yaser7860 <90189815+Yasser-Slaymen@users.noreply.github.com>
2021-12-22 14:07:27 +0100

    index links bewerken

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,7 @@ btn.addEventListener("click",function(){
 
 const boxes=document.querySelectorAll(".box")
 window.addEventListener(("scroll"),checkBoxes)
-checkBoxes()
+
 
 function checkBoxes(){
  const triggerBottom=window.innerHeight /5*4
